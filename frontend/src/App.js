60f3f510f5a2219489e34694cfa8ced3d5a2dc88
yongjun-shin <syj1634@naver.com>
2023-06-15 13:12:32 +0900

    Feat: My Page에 좌측에 있는 Navigation 추가적으로 구현. Test 하기 위해서 history.js, member.js, physiognomy.js 생성

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import Navi from '../src/components/nav.js';
 import Footer from '../src/components/footer.js';
+import Nav_my from './components/nav_mypage.js';
 import {Btn_black, Btn_beige, Btn_home} from './components/button.js';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home/home.js';
@@ -8,6 +9,11 @@ import Pricing from './pages/Pricing/pricing.js';
 import QnA from './pages/QnA/qna.js';
 import Login from './pages/Login/login.js';
 import Join from './pages/Join/join.js';
+
+import MemberInfo from './pages/MemberInfo/member.js';
+import Physiognomy from './pages/Physiognomy/physiognomy.js';
+import CreateImage from './pages/CreateImage/createimage.js';
+import History from './pages/History/history.js';
 import './App.css';
 
 function App() {
@@ -19,6 +25,7 @@ function App() {
         <BrowserRouter>
             <div>
                 <Navi/>
+                <Nav_my/>
                 <Routes>
                     <Route path="/" element={<Home/>} />
                     <Route path="/detect" element={<FaceDetect/>} />
@@ -26,6 +33,11 @@ function App() {
                     <Route path="/qna" element={<QnA/>} />
                     <Route path="/login" element={<Login/>} />
                     <Route path="/join" element={<Join/>} />
+
+                    <Route path="/member" element={<MemberInfo/>} />
+                    <Route path="/physiognomy" element={<Physiognomy/>} />
+                    <Route path="/createimage" element={<CreateImage/>} />
+                    <Route path="/history" element={<History/>} />
                 </Routes>
                 <div>
                     <img src={process.env.PUBLIC_URL + '/test.jpg'}></img>
