cc888e82362dfea9e214ba8cbfbc2232aa4fdd8b
yongjun-shin <syj1634@naver.com>
2023-06-17 21:09:20 +0900

    Feat: pricing.js / pricing.css 구현 완료, radio 버튼 클릭에 따라 box의 border 수정됨.

--- frontend/src/pages/Pricing/pricing.js
+++ frontend/src/pages/Pricing/pricing.js
@@ -1,7 +1,14 @@
 import './pricing.css'
 import Btn_black from '../../components/button';
+import React, { useState } from 'react';
 
 export function Pricing() {
+    const [selectedBox, setSelectedBox] = useState('');
+
+    const handleRadioChange = (boxId) => {
+        setSelectedBox(boxId);
+    };
+
     return (
         <div class='container'>
             <div class='title'>
@@ -9,14 +16,53 @@ export function Pricing() {
             </div>
             <div class='title'>
                 <div class='radio'>
-                    <div class='std'>
-                        <div class='box' style={{backgroundColor:'#EBD4D4'}}></div>
+                    <div class={`box ${selectedBox === 'std' ? 'checked' : ''}`} style={{backgroundColor:'#EBD4D4'}}>
+                        <div class='box_in'>
+                            <input type='radio' id='std' name='radio_group' checked={selectedBox==='std'} onChange={() => handleRadioChange('std')}/>
+                            <div>
+                                <div class='radio_label'>
+                                    <span style={{fontWeight:'900', fontSize:'20px'}}>
+                                        Standard
+                                    </span>
+                                    $1.00/per
+                                </div>
+                                나에게 맞는 화장법을 기반으로한 이미지 제공 및 화장법 소개
+                                <br />
+                                24시간 이내 제공
+                            </div>
+                        </div>
                     </div>
-                    <div class='exp'>
-                        <div class='box' style={{backgroundColor:'#EDC3C3'}}></div>
+                    <div class={`box ${selectedBox === 'exp' ? 'checked' : ''}`} style={{backgroundColor:'#EDC3C3'}}>
+                        <div class='box_in'>
+                            <input type='radio' id='exp' name='radio_group' checked={selectedBox==='exp'} onChange={() => handleRadioChange('exp')}/>
+                            <div>
+                                <div class='radio_label'>
+                                    <span style={{fontWeight:'900', fontSize:'20px'}}>
+                                        Express
+                                    </span>
+                                    $2.00/per
+                                </div>
+                                Standard + 화장품 추천 및 관상 소개
+                                <br />
+                                12시간 이내 제공
+                            </div>
+                        </div>
                     </div>
-                    <div class='prm'>
-                        <div class='box' style={{backgroundColor:'#E9ABAB'}}></div>
+                    <div class={`box ${selectedBox === 'pre' ? 'checked' : ''}`} style={{backgroundColor:'#E9ABAB'}}>
+                        <div class='box_in'>
+                            <input type='radio' id='pre' name='radio_group' checked={selectedBox==='pre'} onChange={() => handleRadioChange('pre')}/>
+                            <div>
+                                <div class='radio_label'>
+                                    <span style={{fontWeight:'900', fontSize:'20px'}}>
+                                        Premium
+                                    </span>
+                                    $3.00/per
+                                </div>
+                                Standard + 화장품 추천 및 관상 소개
+                                <br />
+                                1시간 이내 제공
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
