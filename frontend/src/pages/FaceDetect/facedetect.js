ae01a5f485e6d5818c0183b0ae7c9c03fced7773
pacif1c0 <parkjy318@gmail.com>
2023-06-20 17:45:31 +0900

    Edit Face_Detect 리엑트스럽게 수정

--- frontend/src/pages/FaceDetect/facedetect.js
+++ frontend/src/pages/FaceDetect/facedetect.js
@@ -1,6 +1,6 @@
-import React from "react";
 import './facedetect.css';
-import Btn_black from '../../components/button.js';
+import Btn_black from '/Users/jyp/Desktop/KT_BigProject_07/frontend/src/components/button.js';
+import React, { useState } from "react";
 
 function FaceDetect() {
     return(
@@ -19,6 +19,36 @@ function Text(){
         </div>
     );
 }
+function RadioSet(props){
+    const [selectedValue, setSelectedValue] = useState(""); // 선택된 값을 저장할 상태
+
+    const handleRadioChange = (event) => {
+      setSelectedValue(event.target.value); // 선택된 값 업데이트
+      console.log("selected Value:", event.target.value);
+    };
+
+    return(
+        <div class="radioset1">
+            <div class="radio-container">
+                <label class="label_content1">
+                <div class="radio-content">
+                    <input class="radio_btn_content1" type="radio" name={props.name} value={props.value1} checked={selectedValue === props.value1} // 선택된 값과 비교하여 체크 여부 결정
+                        onChange={handleRadioChange}/>
+                    <p class="text_content1">{props.content1}</p>
+                </div>
+                </label>
+
+                <label class="label_content2">
+                <div class="radio-content">
+                    <input class="radio_btn_content2" type="radio" name={props.name} value={props.value2} checked={selectedValue === props.value2} // 선택된 값과 비교하여 체크 여부 결정
+                        onChange={handleRadioChange}/>
+                    <p class="text_content2">{props.content2}</p>
+                </div>
+                </label>
+            </div>
+        </div>
+    )
+}
 
 function Body(){
     return(
@@ -27,88 +57,21 @@ function Body(){
                 <div class="upload_form">
                     <input type="file" class="upload_photoform" accept="image/*" id="userPhoto" name="userPhoto"/>
                 </div>
+
                 <p class="text1">당신의 피부타입을 알려주세요.</p>
 
                 <p class="text_oil_dry_balance">유수분 밸런스</p>
-
-                <div class="radioset1">
-                    <div class="radio-container">
-                        <label class="label_dry">
-                        <div class="radio-content">
-                            <input class="radio_btn_dry" type="radio" name="D_or_O" value="dry"/>
-                            <p class="text_dry">D : 건성</p>
-                        </div>
-                        </label>
-
-                        <label class="label_oily">
-                        <div class="radio-content">
-                            <input class="radio_btn_oily" type="radio" name="D_or_O" value="oily" />
-                            <p class="text_oily">O : 지성</p>
-                        </div>
-                        </label>
-                    </div>
-                </div>
+                <RadioSet content1="D : 건성" content2="O : 지성" name="D_or_O" value1="Dry" value2="Oily"></RadioSet>
 
                 <p class="text_sensitivity">피부 민감도</p>
-
-                <div class="radioset2">
-                    <div class="radio-container2">
-                        <label class="label_sensitive">
-                        <div class="radio-content">
-                            <input class="radio_btn_sensitive" type="radio" name="S_or_R" value="sensitive" />
-                            <p class="text_sensitive">S : 민감성</p>
-                        </div>
-                        </label>
-
-                        <label class="label_resistance">
-                        <div class="radio-content">
-                            <input class="radio_btn_resistance" type="radio" name="S_or_R" value="resistance" />
-                            <p class="text_resistance">R : 저항성</p>
-                        </div>
-                        </label>
-                    </div>
-                </div>
-                
+                <RadioSet content1="S: 민감성" content2="R : 저항성" name="S_or_R" value1="Sensitive" value2="Resistant"></RadioSet>
 
                 <p class="text_melanin">멜라닌 색소 활성도</p>
-
-                <div class="radioset3">
-                    <div class="radio-container3">
-                        <label class="label_pigment">
-                        <div class="radio-content">
-                            <input class="radio_btn_pigment" type="radio" name="P_or_N" value="pigment" />
-                            <p class="text_pigment">P : 민감성</p>
-                        </div>
-                        </label>
-
-                        <label class="label_nonpigment">
-                        <div class="radio-content">
-                            <input class="radio_btn_nonpigment" type="radio" name="P_or_N" value="nonpigment" />
-                            <p class="text_nonpigment">N : 비색소성</p>
-                        </div>
-                        </label>
-                    </div>
-                </div>
+                <RadioSet content1="P : 민감성" content2="N : 비색소성" name="P_or_N" value1="Pigment" value2="Non_pigment"></RadioSet>
 
                 <p class="text_tension">피부 탄력도 및 주름</p>
-
-                <div class="radioset4">
-                    <div class="radio-container4">
-                        <label class="label_wrinkle">
-                        <div class="radio-content">
-                            <input class="radio_btn_wrinkle" type="radio" name="W_or_T" value="wrinkle" />
-                            <p class="text_wrinkle">W : 주름</p>
-                        </div>
-                        </label>
-
-                        <label class="label_tight">
-                        <div class="radio-content">
-                            <input class="radio_btn_tight" type="radio" name="W_or_T" value="Tight" />
-                            <p class="text_tight">T : 탱탱함</p>
-                        </div>
-                        </label>
-                    </div>
-                </div>
+                <RadioSet content1="W : 주름" content2="T : 탱탱함" name="W_or_T" value1="Wrinkle" value2="Tight"></RadioSet>
+                
                 <Btn_black class="btn_StartAnalysis" text={'분석시작'} style={{width:'237px', height:'46px', fontSize:'20px', marginBottom:'100px'}}></Btn_black>
             </div>
         </div>
