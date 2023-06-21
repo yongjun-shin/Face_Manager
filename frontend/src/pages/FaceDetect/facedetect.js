e8909e05e8cfeccbfa13905c5cba8bf79520e769
pacif1c0 <parkjy318@gmail.com>
2023-06-21 23:29:25 +0900

    fix: pricing.css 디자인 버그 수정, facedetect 라디오버튼 디자인 변경

--- frontend/src/pages/FaceDetect/facedetect.js
+++ frontend/src/pages/FaceDetect/facedetect.js
@@ -41,7 +41,7 @@ function NewRadio(props){
                 <label class='label_content1'>
                     <div class="radio-content1" style={{backgroundColor: props.bg_color1}}>
                         <input class="RadioBtn_content1" type='radio' name={props.name} value={props.value1} checked={selectedValue === props.value1}
-                            onChange={handleRadioChange}></input>
+                            onChange={handleRadioChange} style={{accentColor: props.accentColor}}></input>
                         <div class="content1">
                             <p class="text_top1">{props.text_top1}</p>
                             <p class="text_center1">{props.text_center1}</p>
@@ -60,7 +60,7 @@ function NewRadio(props){
                             <p class="text_bottom2">{props.text_bottom2}</p>
                         </div>
                         <input class="RadioBtn_content2" type='radio' name={props.name} value={props.value2} checked={selectedValue === props.value2}
-                            onChange={handleRadioChange}></input>
+                            onChange={handleRadioChange} style={{accentColor: props.accentColor}}></input>
                     </div>
                 </label>
             </div>
@@ -79,14 +79,14 @@ function Body(){
                 <p class="text1">당신의 피부타입을 알려주세요.</p>
 
                 <RadioTitle text_color="#537aa5" content="유수분 밸런스"></RadioTitle>
-                <NewRadio name="D_or_O" 
+                <NewRadio name="D_or_O" accentColor="#537aa5"
                     value1="Dry" text_top1="D" text_center1="건성" text_bottom1="Dry"
                     description1="피지 분비량과 수분 보유량 모두 적어 거칠고 각질과 주름이 잘 생기는 타입" bg_color1="#E9EEF4"
                     value2="Oily" text_top2="O" text_center2="지성" text_bottom2="Oily"
                     description2="피지 분비량이 많아 번들거리고 여드름이 자주 생기는 타입" bg_color2="#D5DBE9"></NewRadio>
 
                 <RadioTitle text_color="#7E7F83" content="피부 민감도"></RadioTitle>
-                <NewRadio name="S_or_R" 
+                <NewRadio name="S_or_R" accentColor="#7E7F83"
                     value1="Sensitive" text_top1="S" text_center1="민감성" text_bottom1="Sensitive"
                     description1="피부가 얇고 섬세해 외부 자극에 쉽게 반응하는 타입" bg_color1="#EEEEF0"
                     value2="Resistant" text_top2="R" text_center2="저항성" text_bottom2="Resistant"
@@ -94,14 +94,14 @@ function Body(){
                
 
                <RadioTitle text_color="#288E9A" content="멜라닌 색소 활성도"></RadioTitle>
-                <NewRadio name="P_or_N" 
+                <NewRadio name="P_or_N" accentColor="#288E9A"
                     value1="Pigment" text_top1="P" text_center1="민감성" text_bottom1="Pigment"
                     description1="멜라닌 활성도가 높아 기미, 주근깨 혹은 잡티 등 눈에 보이는 색소가 많은 타입" bg_color1="#EAEFF3"
                     value2="Non-Pigment" text_top2="N" text_center2="비색소성" text_bottom2="Non-Pigment"
                     description2="멜라닌 활성도가 낮아 눈에 보이는 색소가 적은 타입" bg_color2="#D3E0E6"></NewRadio>
                 
                 <RadioTitle text_color="#515FA8" content="피부 탄력도 및 주름"></RadioTitle>
-                <NewRadio name="W_or_T" 
+                <NewRadio name="W_or_T" accentColor="#515FA8"
                     value1="Wrinkle" text_top1="W" text_center1="주름" text_bottom1="Wrinkle"
                     description1="피부 결이 고르지 않고 주름이 많은 타입" bg_color1="#E8E8F4"
                     value2="Tight" text_top2="T" text_center2="탱탱함" text_bottom2="Tight"
