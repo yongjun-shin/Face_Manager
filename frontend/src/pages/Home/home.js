f7fa46ab0e969abd669cbf30c9987ed0043b6416
ChaeHyun-Lee <dlcogus99@naver.com>
2023-06-23 15:01:51 +0900

    Add slide components

--- frontend/src/pages/Home/home.js
+++ frontend/src/pages/Home/home.js
@@ -1,10 +1,12 @@
 import React from "react";
 import TextBox from "../../components/textbox.js"
 import TextBox2 from "../../components/textbox2.js"
+import Slide from "../../components/slide/slide.js"
 
 function Main(){
     return(
     <div className="wrapper" styles={{zoom:0.5, marginTop: "100px"}}>
+        <Slide></Slide>
         <TextBox2
         imageUrl="https://i.pinimg.com/474x/45/26/ac/4526ac70ec7c25ae7962a7da072e32f2.jpg"
         title="Face Detect"
