8a0be8f9fca23775f4fdacc40f11ea5734c05b6a
yongjun-shin <syj1634@naver.com>
2023-07-02 04:02:30 +0900

    Fix : 회원정보 페이지 DB에서 끌어올 정보가 없을때의 상태 조건문으로 예외 처리 완료

--- frontend/src/pages/MemberInfo/member.js
+++ frontend/src/pages/MemberInfo/member.js
@@ -1,25 +1,54 @@
 import styled from "styled-components";
 import Nav_my from '../../components/nav_mypage.js';
+import { useNavigate } from "react-router-dom";
 import { ReactComponent as MemberImg } from '../svgs/member_info.svg';
+import { Btn_home } from "../../components/button.js";
 import './member.css';
 
 function Boxes() {
     const data = [
-      {
-        date: '2023-05-08 09:35',
-        payment: 'Express',
-        method: '신용카드',
-        amount: '$2.00/per',
-      },
-      {
-        date: '2023-06-08 14:35',
-        payment: 'Premium',
-        method: '계좌이체',
-        amount: '$3.00/per',
-      },
-      
+      // {
+      //   date: '2023-05-08 09:35',
+      //   payment: 'Express',
+      //   method: '신용카드',
+      //   amount: '$2.00/per',
+      // }
     ];
-  
+    let pay_history = null;
+    if(data.length === 0){
+      pay_history = (<div className="box">
+        <div className="box_in">
+            <p>None</p>
+        </div>
+        <div className="box_in">
+            <p>None</p>
+        </div>
+        <div className="box_in">
+            <p>None</p>
+        </div>
+        <div className="box_in">
+            <p>None</p>
+        </div>
+      </div>);
+    } else {
+      pay_history = (data.map((item, index) => (
+        <div className="box" key={index}>
+          <div className="box_in">
+            <p>{item.date}</p>
+          </div>
+          <div className="box_in">
+            <p>{item.payment}</p>
+          </div>
+          <div className="box_in">
+            <p>{item.method}</p>
+          </div>
+          <div className="box_in">
+            <p>{item.amount}</p>
+          </div>
+        </div>
+      )));
+    }
+
     return (
       <div className="box_con">
         <div className="box">
@@ -37,73 +66,110 @@ function Boxes() {
             </div>
         </div>
         <hr style={{width: '780px', float:'left', marginTop:'0px', marginBottom:'0px'}}/>
-        {data.map((item, index) => (
-          <div className="box" key={index}>
-            <div className="box_in">
-              <p>{item.date}</p>
-            </div>
-            <div className="box_in">
-              <p>{item.payment}</p>
-            </div>
-            <div className="box_in">
-              <p>{item.method}</p>
-            </div>
-            <div className="box_in">
-              <p>{item.amount}</p>
-            </div>
-          </div>
-        ))}
+        {pay_history}
       </div>
     );
   }
 
+const P_skin = styled.p`
+  text-align: center;
+  font-size: 18px;
+  margin-bottom: 12px;
+`;
+const Div = styled.div`
+  display: flex;
+  justify-content: center;
+`
 function Skin() {
   let type = null;
-  type = 'DSNT';
+  let type2 = 'dsnt';
+  //type = 'dsnt';
   let color = ['#537AA5', '#7E7F83', '#288E9A', '#515FA8'];
   let content = [];
+  let content2 = ['피지 분비량과 수분 보유량 모두 적어 거칠고 각질과 주름이 잘 생기는 타입', '피부가 얇고 섬세해 외부 자극에 쉽게 반응하는 타입', '멜라닌 활성도가 낮아 눈에 보이는 색소가 적은 타입', '피부 결이 고르고 주름이 적어 탄력이 있는 타입']
+  let skin_history = null;
 
-  if(type[0] === 'D'){
-    content.push('피지 분비량과 수분 보유량 모두 적어 거칠고 각질과 주름이 잘 생기는 타입');
+  const navigate = useNavigate();
+  const scrollToTop = () => {
+    window.scrollTo(0, 0);
   }
-  else if(type[0] === 'O'){
-    content.push('피지 분비량이 많아 번들거리고 여드름이 자주 생기는 타입');
-  }
-
-  if(type[1] === 'S'){
-    content.push('피부가 얇고 섬세해 외부 자극에 쉽게 반응하는 타입');
-  }
-  else if(type[1] === 'R'){
-    content.push('피부 장벽이 견고해 외부적인 스트레스에 대해 견디는 힘이 강한 타입');
+  const btn_click = () => {
+    scrollToTop();
+    navigate('/detect');
   }
 
-  if(type[2] === 'N'){
-    content.push('멜라닌 활성도가 낮아 눈에 보이는 색소가 적은 타입');
-  }
-  else if(type[2] === 'P'){
-    content.push('멜라닌 활성도가 높아 기미, 주근깨 혹은 잡티 등 눈에 보이는 색소가 많은 타입');
-  }
+  if(type === null){
+    skin_history = (
+      <div>
+        <div style={{display:'flex', alignItems:'center', marginBottom:'20px', width:'680px'}}>
+          <div>
+            <P_skin>Face Detect 페이지에서 피부타입을 선택해주세요</P_skin>
+            <P_skin>선택한 피부타입 정보를 보여드립니다</P_skin>
+          </div>  
+          <Btn_home text='Face Detect 바로가기' style={{fontSize:'15px', width:'250px', height:'40px',marginLeft:'25px'}} onClick={btn_click}/>
+        </div>
+        <Div>
+          <div>
+            <p style={{marginBottom:'10px'}}>예시)</p>
+            <div style={{marginLeft:'25px', height:'170px', display:'flex', justifyContent:'space-between', flexDirection:'column'}}>
+              {content2.map((item, index) => (
+                <div key={index} className='line'>
+                  <div className="circle" style={{backgroundColor: color[index%color.length], fontSize:'20px', width:'30px', height:'30px'}}>
+                    <p>{type2[index].toUpperCase()}</p>
+                  </div>
+                  <p>{item}</p>
+                </div>
+              ))}
+            </div>
+          </div>
+        </Div>
+      </div>
+    );
+  } else {
+      if(type[0] === 'd'){
+        content.push('피지 분비량과 수분 보유량 모두 적어 거칠고 각질과 주름이 잘 생기는 타입');
+      }
+      else if(type[0] === 'o'){
+        content.push('피지 분비량이 많아 번들거리고 여드름이 자주 생기는 타입');
+      }
+    
+      if(type[1] === 's'){
+        content.push('피부가 얇고 섬세해 외부 자극에 쉽게 반응하는 타입');
+      }
+      else if(type[1] === 'r'){
+        content.push('피부 장벽이 견고해 외부적인 스트레스에 대해 견디는 힘이 강한 타입');
+      }
+    
+      if(type[2] === 'n'){
+        content.push('멜라닌 활성도가 낮아 눈에 보이는 색소가 적은 타입');
+      }
+      else if(type[2] === 'p'){
+        content.push('멜라닌 활성도가 높아 기미, 주근깨 혹은 잡티 등 눈에 보이는 색소가 많은 타입');
+      }
+    
+      if(type[3] === 't'){
+        content.push('피부 결이 고르고 주름이 적어 탄력이 있는 타입');
+      }
+      else if(type[3] === 'w'){
+        content.push('피부 결이 고르지 않고 주름이 많은 타입');
+      }
 
-  if(type[3] === 'T'){
-    content.push('피부 결이 고르고 주름이 적어 탄력이 있는 타입');
-  }
-  else if(type[3] === 'W'){
-    content.push('피부 결이 고르지 않고 주름이 많은 타입');
+    skin_history = (
+      content.map((item, index) => (
+        <div key={index} className='line'>
+          <div className="circle" style={{backgroundColor: color[index%color.length]}}>
+            <p>{type[index].toUpperCase()}</p>
+          </div>
+          <p>{item}</p>
+        </div>
+      ))
+    );
   }
 
   return (
     <div className="skin">
       <div className="skin_con">
-      {
-        content.map((item, index) => (
-          <div key={index} className='line'>
-            <div className="circle" style={{backgroundColor: color[index%color.length]}}>
-              <p>{type[index]}</p>
-            </div>
-            <p>{item}</p>
-          </div>
-        ))
-      }
+        {skin_history}
       </div>
     </div>
   );
@@ -132,7 +198,7 @@ export function MemberInfo() {
     }
     let rank = null;
     if (rank === null){
-        rank = 'Premium';
+        rank = 'None';
     }
 
     return (
